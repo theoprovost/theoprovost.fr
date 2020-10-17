6594f50a737b2487c965351bc585e3e9f86cf35f
Théo Provost <theo.provost@gmail.com>
2020-10-17 16:13:21 +0200

    Debug H-13

--- app/server.js
+++ app/server.js
@@ -24,4 +24,10 @@ app.use(router);
 
 server.listen(port, () => console.log(`Server is listening on https://localhost:${port}`));
 
+// Debug H-13
+server.on('clientError', (err, socket) => {
+    console.error(err);
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+  });
+
 module.exports = server;
