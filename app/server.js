3f8dde20dac07717222f0c1c854844b414c1e414
Théo Provost <theo.provost@gmail.com>
2020-10-17 16:24:17 +0200

    Deploy test w/ HTTP protocol

--- app/server.js
+++ app/server.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = require('./router');
 
-const https = require('https');
-const fs = require('fs');
+// const https = require('https');
+// const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 443;
 
 
-const server = https.createServer({
-    key: fs.readFileSync(__dirname + '/ssl/server.key'),
-    cert: fs.readFileSync(__dirname + '/ssl/server.cert')
-}, app);
+// const server = https.createServer({
+//     key: fs.readFileSync(__dirname + '/ssl/server.key'),
+//     cert: fs.readFileSync(__dirname + '/ssl/server.cert')
+// }, app);
 
 app.use(express.json());
 app.use(cors({
-    origin: ['https://theoprovost.herokuapp.com'],
+    origin: ['https://theoprovost.herokuapp.com', 'http://localhost'],
     optionsSuccessStatus: 200 // for legacy brower support
 }));
 
 app.use(router);
 
-server.listen(port, () => console.log(`Server is listening on https://localhost:${port}`));
+app.listen(port, () => console.log(`Server is listening on https://localhost:${port}`));
 
 // Debug H-13
-server.on('clientError', (err, socket) => {
+app.on('clientError', (err, socket) => {
     console.error(err);
     socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
   });
