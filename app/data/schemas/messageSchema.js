53b27b6b2c26a9f7d5d9d3874e269297c449d658
Théo Provost <theo.provost@gmail.com>
2020-10-19 01:59:22 +0200

    Modified message Schema and contact class done

--- app/data/schemas/messageSchema.js
+++ app/data/schemas/messageSchema.js
@@ -3,10 +3,8 @@ const Joi = require('joi');
 const messageSchema = Joi.object({
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
-    organization: Joi.string(),
     email: Joi.string().email().required(),
-    telephone: Joi.string(), // TO DO : add regexp pattern 
-    title: Joi.string(),
+    telephone: Joi.string().required(),
     content: Joi.string().required() 
 });
 
---
+++ app/models/contact.js
@@ -0,0 +1,23 @@
+const db = require('../data/database');
+
+class Contact {
+    constructor(data) {
+        for (const prop in data) {
+            this[prop] = data[prop];
+        }
+    };
+
+    async save() {
+        const sendContactForm = await db.query(`
+        INSERT INTO contact(first_name, last_name, email, telephone, content)
+        VALUES($1, $2, $3, $4, $5)
+        RETURNING contact.id;
+        `, [this.first_name, this.last_name, this.email, this.telephone, this.content]);
+
+        if (sendContactForm.rowCount) {
+            this.id = sendContactForm.rows[0].id;
+        };
+    };
+};
+
+module.exports = Contact;
