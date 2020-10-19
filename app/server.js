955b9f8221064ac7e31e197f0a14bf3c1ae7edbe
Théo Provost <theo.provost@gmail.com>
2020-10-19 03:30:45 +0200

    ERR H-10 debug

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const router = require('./router');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
