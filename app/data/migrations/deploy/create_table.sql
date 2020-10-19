14b3656817d314605a305a47633504867ca4975a
Théo Provost <theo.provost@gmail.com>
2020-10-19 03:10:54 +0200

    Database connected

--- app/data/migrations/deploy/create_table.sql
+++ app/data/migrations/deploy/create_table.sql
@@ -3,12 +3,12 @@
 BEGIN;
 
 CREATE TABLE "contact" (
-    id INT GENERTAED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
     email VARCHAR NOT NULL,
     telephone VARCHAR NOT NULL,
-    content TEXT NOT NULL,
+    "content" TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ,
     deleted_at TIMESTAMPTZ
