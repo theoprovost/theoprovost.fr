1e569a768df7e1b93f6031ccb0c3588c49f768cb
Théo Provost <theo.provost@gmail.com>
2020-10-19 02:08:41 +0200

    PG Database client

--- app/data/database.js
+++ app/data/database.js
@@ -1,1 +1,12 @@
-// DB client here
+const { Pool } = require('pg');
+
+let clientPg;
+if (process.env.NODE_ENV && process.env.NODE_ENV === "production") { 
+    clientPg = new Pool({
+        connectionString: process.env.DATABASE_URL
+    });
+} else { 
+    clientPg = new Pool();
+}
+
+module.exports = clientPg;
